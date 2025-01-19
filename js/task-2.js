6bd882d5cbd5770404851f201fabf340fd0a5d3f
Denis Savranin <savranindenisv@gmail.com>
2025-01-20 00:17:14 +0200

    fixed task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,26 +1,10 @@
-// Задача 2. Композиція масивів (1-й варіант з використанням slice)
-
+// Задача 2. Композиція масивів
 function makeArray(firstArray, secondArray, maxLength) {
   let totalArray = firstArray.concat(secondArray);
-  return totalArray.slice(0, maxLength);
-}
-
-console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
-
-// Задача 2. Композиція масивів (2-й варіант з використанням push)
-
-function makeArray(firstArray, secondArray, maxLength) {
-  let totalArray = firstArray.concat(secondArray);
-  const resultArray = [];
-  for (let index = 0; index < maxLength; index++) {
-    resultArray.push(totalArray[index]);
+  if (totalArray.length > maxLength) {
+    return totalArray.slice(0, maxLength);
   }
-  return resultArray;
+  return totalArray;
 }
 
 console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
